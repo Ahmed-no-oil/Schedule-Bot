748bdfd1b6f8cfcca710cf328c512b1cc34e35c8
ahmed-no-oil <ahmed.91.no.oil@gmail.com>
2023-06-07 01:07:22 +0300

    * Schedule Controller * A model for HTTP Ok response body

---
+++ src/main/kotlin/com/ahmed/schedulebot/controllers/ScheduleController.kt
@@ -0,0 +1,26 @@
+package com.ahmed.schedulebot.controllers
+
+import com.ahmed.schedulebot.entities.ScheduleEntry
+import com.ahmed.schedulebot.models.ScheduleDay
+import com.ahmed.schedulebot.services.ScheduleDataService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+
+@RestController
+@RequestMapping("/api/schedule")
+class ScheduleController(val dataService: ScheduleDataService) {
+    @GetMapping("/get_this_week_data")
+    fun getThisWeekData(): ResponseEntity<List<ScheduleDay>> {
+        val weekNumber: Int
+        Calendar.getInstance().let {
+            weekNumber = it[Calendar.WEEK_OF_YEAR]
+        }
+        val responseData = dataService.findWeekData(weekNumber)?.map { ScheduleDay().of(it) } ?: return ResponseEntity.notFound().build()
+        return ResponseEntity.ok(responseData)
+    }
+}
