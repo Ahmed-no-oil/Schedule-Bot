301cfb4bdf30279c300626ab2eae3031b4578356
ahmed-no-oil <ahmed.91.no.oil@gmail.com>
2024-03-27 01:53:41 +0100

    In the testing: include verification that no caching occurred

--- src/test/kotlin/com/ahmed/schedulebot/ScheduleBotApplicationTests.kt
+++ src/test/kotlin/com/ahmed/schedulebot/ScheduleBotApplicationTests.kt
@@ -7,6 +7,7 @@ import com.ahmed.schedulebot.services.ImageService
 import com.ahmed.schedulebot.services.ScheduleImageBuilder
 import io.mockk.mockk
 import io.mockk.every
+import io.mockk.verify
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.util.AssertionErrors.assertEquals
@@ -23,16 +24,20 @@ class ImageServiceTests {
 
     @Test
     fun givenNoScheduleData_whenCallingGetImage_thenNoImageIsCached() {
-        // given
+        // given:
         val arbitraryYear = 2024
         val arbitraryWeekNumber =1
         every { weekRepository.findByYearAndWeekNumber(arbitraryYear, arbitraryWeekNumber) } returns null
         every { scheduleEntryRepository.findByWeek(Week(arbitraryYear, arbitraryWeekNumber)) } returns null
         val imageService = ImageService(scheduleEntryRepository,weekRepository,scheduleImageBuilder)
-        // when
-        val result: ByteArray = imageService.getImage(arbitraryYear, arbitraryWeekNumber)
 
-        // then
+        // when:
+        val result: ByteArray = imageService.getImage(arbitraryYear, arbitraryWeekNumber)
+        imageService.getImage(arbitraryYear, arbitraryWeekNumber)
+
+        // then:
         assertEquals("The byte array from getImage", 0, result.size)
+        //Verify that no caching has happened
+        verify(exactly = 2) { weekRepository.findByYearAndWeekNumber(arbitraryYear, arbitraryWeekNumber) }
     }
 }
